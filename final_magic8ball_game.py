b63d666bd8b36873137d3c05cf76e76dc00744f6
Prathamm Surana <prathammsurana1@gmail.com>
2024-11-12 17:09:57 +0000

    function name update

--- final_magic8ball_game.py
+++ final_magic8ball_game.py
@@ -1,26 +1,27 @@
 
 import random
-from colorama import Fore, Style, Back #* we have imported 'colorama' and 'random' library and used their functions
+from colorama import Fore, Style  #* we have imported 'colorama' and 'random' library and used their functions
 
-def function():    #* we are defining a function that will ask a question, that generates a random number and chooses an item from the relevant list according to set condition.
+def game_function():    #* we are defining a function that will ask a question, that generates a random number and chooses an item from the relevant list according to set condition.
  input("What is your question?: ")   #user input 
 
  rdm_num = (random.randint(1,2))   # generation of random number between integers (1,2)- 50% chances of odd/even
- #print(rdm_num)
+ #print(rdm_num)  # to check the number for reference
 #* here are two lists, one good and bad, from which the program will randomly select depending on the number generated
- good_response = ["It is certain", # A list of common good responses for a magic 8ball
+ # A list of common good responses for a magic 8ball
+ good_response = ["It is certain", 
                  "It is decidedly so",
                  "Without a doubt",
                  "Yes definitely",
                  "You may rely on it"]
-
- bad_response = ["Don't count on it", # A list of common bad responses for a magic 8ball
+ # A list of common bad responses for a magic 8ball
+ bad_response = ["Don't count on it", 
                 "My reply is no",
                 "My sources say no",
                 "Outlook not so good",
                 "Very doubtful",
                 ]
-
+ 
  even_num = 2
 # Ron suggested even_num = 2 and the randint be (1,3) allowing us to easily determine an odd or even number, 
 # After discussion it was suggested that 'randint' being (1,2) would allow for a fairer game, while accomplishing the same task.
@@ -41,7 +42,7 @@ def function():    #* we are defining a function that will ask a question, that 
    print("Invalid entry. Please type Y/N")
    repeat= input("Do you want to ask another question? (Y/N): ").strip().lower()
  if repeat == "y":  # revisit the initial function and repeat the whole process
-  function ()
+  game_function ()
  elif repeat == "n":  # exit the program. Will use ascii
   print(r"""       Thank You 
          .-'-.
@@ -52,6 +53,5 @@ def function():    #* we are defining a function that will ask a question, that 
       For Playing""")   # used ascii to show 8 ball.
 # it gives this error message "unterminated string literal" as it cant ignore escape characters such as \.
 # to avoid this, we used the "raw strings" prefix "r", "r" will help maintain proper formatting, while using triple quotes (""") handles multi-line strings.
-
  
-function() # calling function() to start the program
+game_function() # calling function() to start the program
